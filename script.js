4595d1a75d8a355b41e63129fbf49da7f06af09f
snehaingle <snehaengle195@gmail.com>
2024-12-06 13:08:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+var x, y, result; 
+
+function input1(){
+    document.getElementById("inputbox").value += "1" ;
+}
+function input2(){
+    document.getElementById("inputbox").value += "2" ;
+}
+function input3(){
+    document.getElementById("inputbox").value += "3" ;
+}
+function inputadd(){
+    document.getElementById("inputbox").value += "+" ;
+}
+function input4(){
+    document.getElementById("inputbox").value += "4" ;
+}
+function input5(){
+    document.getElementById("inputbox").value += "5" ;
+}
+function input6(){
+    document.getElementById("inputbox").value += "6" ;
+}
+function inputminus(){
+    document.getElementById("inputbox").value += "minus" ; 
+}
+function calculate( ) {
+
+document.getElementById("inputbox").value = eval
+(document.getElementById("inputbox").value);
+}
+
+
+ 
+
+
+
+
+
+
